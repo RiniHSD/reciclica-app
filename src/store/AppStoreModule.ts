1b322fc6dfa34bf507461a8c08f18b36af5cb9f5
RiniHSD <rinihusadiyah@gmail.com>
2023-09-03 17:53:54 +0700

    recover password functtionally

--- src/store/AppStoreModule.ts
+++ src/store/AppStoreModule.ts
@@ -1,7 +1,9 @@
 import { StoreModule } from "@ngrx/store"
 import { LoadingReducer } from "./loading/loading.reducers"
+import { loginReducer } from "./login/login.reducers"
 
 export const AppStoreModule = [
     StoreModule.forRoot([]),
-    StoreModule.forFeature("loading", LoadingReducer)
+    StoreModule.forFeature("loading", LoadingReducer),
+    StoreModule.forFeature("login", loginReducer)
 ]
