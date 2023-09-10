d7896239cce0a01154eaa8845126bdbdaa6a1367
RiniHSD <rinihusadiyah@gmail.com>
2023-09-10 08:20:38 +0700

    state, actions, and reduces

---
+++ src/app/model/address/Address.ts
@@ -0,0 +1,9 @@
+export class Address {
+    address!: string;
+    number!: string;
+    neighborhood!: string;
+    complement!: string;
+    zipCode!: string;
+    state!: string;
+    city!: string;
+}
---
+++ src/store/register/RegisterState.ts
@@ -0,0 +1,5 @@
+export class RegisterState {
+    error: any;
+    isRegistered?: boolean;
+    isRegistering?: boolean;
+}
